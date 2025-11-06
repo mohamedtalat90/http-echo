257cc7d44c18be4deef2910a756a1503067d5161
Mohamed Talat <mohamedsamny@gmail.com>
2025-11-06 11:07:54 +0200

    feat(terraform): add new variables for server configuration and GitOps setup

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,3 +1,37 @@
+variable "hcloud_token" {
+  type      = string
+  sensitive = true
+}
+variable "ssh_public_key_path" {
+  type = string
+  # e.g. "~/.ssh/id_rsa.pub"
+}
+variable "server_type" {
+  type    = string
+  default = "cx33"
+  # pick size
+}
+variable "location" {
+  type    = string
+  default = "nbg1"
+  # or hel1/fsn1
+}
+variable "image" {
+  type    = string
+  default = "ubuntu-24.04"
+}
+variable "hostname" {
+  type    = string
+  default = "k3s-node-1"
+}
+
+# Kubeconfig path on YOUR laptop (where terraform runs)
+variable "kubeconfig_path" {
+  type    = string
+  default = "~/.kube/hetzner-k3s.yaml"
+}
+
+# ArgoCD + app namespaces
 variable "argocd_namespace" {
   type    = string
   default = "argocd"
@@ -6,11 +40,23 @@ variable "app_namespace" {
   type    = string
   default = "http-echo"
 }
+
+# GitOps
 variable "git_repo_url" {
-  type        = string
-  description = "https://github.com/mohamedtalat90/http-echo"
+  type    = string
+  default = "https://github.com/mohamedtalat90/http-echo.git"
 }
 variable "git_revision" {
   type    = string
   default = "main"
+}
+
+# TLS + domains
+variable "base_domain" {
+  type = string
+  # e.g. example.com or <IP>.sslip.io
+}
+variable "acme_email" {
+  type = string
+  # your email for Let's Encrypt
 }
